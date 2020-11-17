eaa269cbb8e89510876ccddc0abedf38fb06d384
Wylkerd <wylkerd@hotmail.com>
2020-11-17 20:57:34 -0300

    Arrow Functions

--- bunble.js
+++ bunble.js
@@ -1,24 +1,30 @@
 "use strict";
 
-var arr = [1, 3, 4, 5, 10]; // Funções para trabalhar com arrays/vetores no ES6
-// Função map() percorre o vetor e retorna uma informação igual ou nova
+var arr = [1, 3, 4, 5, 10]; //function(item) é uma função anônima, melhor lugar para se usar errow function =>
+// quando a função recebe apenas um parâmetro pode-se remove-los (item) para item =>
+// quando só tem um retorno, o mesmo pode ficar desta seguinte forma
 
-var newArr = arr.map(function (item, index) {
-  return (item + index) * 2;
+var newArr = arr.map(function (item) {
+  return item * 2;
 });
-console.log(newArr); // Função reduce() consome todo o vetor e retorna uma única informação
+console.log(newArr); //retorno de string
 
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log(sum); // Função filter() filtra o conteudo do array
+var testefunc = function testefunc() {
+  return 'teste função';
+};
 
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter); // Função find() verifica se exite determinada informção no array
+console.log(testefunc()); //retorno de função
 
-var find = arr.find(function (item) {
-  return item === 4;
-});
-console.log(find);
+var testefunc2 = function testefunc2() {
+  return [1, 2, 3];
+};
+
+console.log(testefunc2()); //retorno de objeto
+
+var testefunc3 = function testefunc3() {
+  return {
+    nome: "Wylkerd"
+  };
+};
+
+console.log(testefunc3());
--- main.js
+++ main.js
@@ -1,34 +1,22 @@
 const arr = [1, 3, 4, 5, 10];
 
-// Funções para trabalhar com arrays/vetores no ES6
-// Função map() percorre o vetor e retorna uma informação igual ou nova
-const newArr = arr.map(function(item, index)
-{
-    return (item + index) * 2;
-}) 
+
+//function(item) é uma função anônima, melhor lugar para se usar errow function =>
+// quando a função recebe apenas um parâmetro pode-se remove-los (item) para item =>
+// quando só tem um retorno, o mesmo pode ficar desta seguinte forma
+const newArr = arr.map(item => item * 2);
 
 console.log(newArr);
 
-// Função reduce() consome todo o vetor e retorna uma única informação
+//retorno de string
+const testefunc = () => 'teste função';
+console.log(testefunc());
 
-const sum = arr.reduce(function(total, next)
-{
-    return total + next;   
-});
+//retorno de função
+const testefunc2 = () => [1, 2, 3];
+console.log(testefunc2());
 
-console.log(sum);
 
-// Função filter() filtra o conteudo do array
-
-const filter = arr.filter(function(item){
-    return item % 2 === 0;
-});
-
-console.log(filter);
-
-// Função find() verifica se exite determinada informção no array
-const find = arr.find(function(item){
-    return item === 4;
-});
-
-console.log(find);
+//retorno de objeto
+const testefunc3 = () => ({ nome: "Wylkerd"});
+console.log(testefunc3());
