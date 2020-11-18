4d0ef715a32bfa5c075f810d9b17099d2e8e39b4
Wylkerd <wylkerd@hotmail.com>
2020-11-17 22:31:09 -0300

    Template literals

--- bunble.js
+++ bunble.js
@@ -1,73 +1,8 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+// Template literals: Forma de incluir variáveis dentro de strings no JavaScript a partir do ES6
+var nome = "wylkerd";
+var idade = 20; // console.log('Meu nome é '+ nome +' e tenho '+ idade + ' anos.');
+// USA-SE CRASE ``
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-
-// REST = Serve para pegar o resto das propriedades
-// Rest operator em objetos
-var usuario = {
-  nome: 'Wylkerd',
-  idade: 20,
-  empresa: 'WylkerDev'
-};
-
-var nome = usuario.nome,
-    resto = _objectWithoutProperties(usuario, ["nome"]);
-
-console.log(nome);
-console.log(resto); // Rest operator em vetores
-
-var arr = [1, 2, 3, 4];
-var a = arr[0],
-    b = arr[1],
-    c = arr.slice(2);
-console.log(a);
-console.log(b);
-console.log(c); // Rest operator em parâmetros de funções
-
-function soma() {
-  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
-    params[_key] = arguments[_key];
-  }
-
-  return params.reduce(function (total, next) {
-    return total + next;
-  });
-}
-
-console.log(soma(1, 3, 4));
-
-function mostraParams(a, b) {
-  for (var _len2 = arguments.length, params = new Array(_len2 > 2 ? _len2 - 2 : 0), _key2 = 2; _key2 < _len2; _key2++) {
-    params[_key2 - 2] = arguments[_key2];
-  }
-
-  return params;
-}
-
-console.log(mostraParams(1, 3, 4, 5)); // SPREAD OPERATOR = Propaga os items de um array para outra estrutura de dados
-// Em arrays
-
-var arr1 = [1, 2, 3];
-var arr2 = [4, 5, 6];
-var arr3 = [].concat(arr1, arr2);
-console.log(arr3); // Em objetos
-
-var usuario1 = {
-  nome: 'Wylkerd',
-  idade: 20,
-  empresa: 'Rocketseat'
-};
-
-var usuario2 = _objectSpread(_objectSpread({}, usuario1), {}, {
-  nome: 'Gabriel'
-});
-
-console.log(usuario2);
+console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos."));
--- main.js
+++ main.js
@@ -1,54 +1,7 @@
-// REST = Serve para pegar o resto das propriedades
-// Rest operator em objetos
-const usuario = {
-    nome: 'Wylkerd',
-    idade: 20,
-    empresa: 'WylkerDev'
-};
+// Template literals: Forma de incluir variáveis dentro de strings no JavaScript a partir do ES6
+const nome = "wylkerd";
+const idade = 20;
 
-const { nome, ...resto } = usuario;
-
-console.log(nome);
-console.log(resto);
-
-// Rest operator em vetores
-const arr= [1 ,2, 3, 4];
-
-const [ a, b, ...c ] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-// Rest operator em parâmetros de funções
-function soma(...params) {
-    return params.reduce((total, next) => total + next);
-}
-
-console.log(soma(1, 3, 4));
-
-function mostraParams(a, b, ...params) {
-    return params;
-}
-
-console.log(mostraParams(1, 3, 4, 5));
-
-// SPREAD OPERATOR = Propaga os items de um array para outra estrutura de dados
-// Em arrays
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [ ...arr1, ...arr2 ];
-
-console.log(arr3)
-
-// Em objetos
-const usuario1 = {
-    nome: 'Wylkerd',
-    idade: 20,
-    empresa: 'Rocketseat',    
-};
-
-const usuario2 = { ...usuario1, nome: 'Gabriel' };
-
-console.log(usuario2);
+// console.log('Meu nome é '+ nome +' e tenho '+ idade + ' anos.');
+// USA-SE CRASE ``
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
