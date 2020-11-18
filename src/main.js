fc44a25f46ee56d6acda8479f4ec770d42265bcc
Wylkerd <wylkerd@hotmail.com>
2020-11-18 20:09:50 -0300

    Async/Await (promise)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-alert('Testando...');
+const minhaPromisse = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('OK')}, 2000);
+}); 
+
+minhaPromisse().then(response => {
+    console.log(response);
+})
