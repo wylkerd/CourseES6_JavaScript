0af2d0f448f36bb79f30cc45d82184bf4b811f0c
Wylkerd <wylkerd@hotmail.com>
2020-11-19 00:15:49 -0300

    Screen rendering

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
         this.repositories = [];
 
         this.formEl = document.querySelector("#repo-form");
+        this.listEl = document.querySelector("#repo-list");
 
         this.registerHandles();
     }
@@ -16,13 +17,41 @@ class App {
         event.preventDefault();
 
         this.repositories.push({
-            name:'rocketseat.com.br',
-            description:'Tire a sua ideia do papel e de vida a sua startup.',
-            avatar_url:'https://avatars0.githubusercontent.com/u/28929274?v4',
-            html_url:'http://github.com/rocketseat/rocketseat.com.br',
+            name:'Wylkerd',
+            description:'Desenvolvimento de modelos preditivos com Python. Desenvolvimento de modelos e APIs.',
+            avatar_url:'https://avatars0.githubusercontent.com/u/50023013?v=4',
+            html_url:'https://github.com/wylkerd',
         });
 
-        console.log(this.repositories);
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach( repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
